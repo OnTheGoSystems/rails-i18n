e6ace8128066ceaf9fa710708f649ddb6d9d0f38
Ivan Larionov <ivan.l@onthegosystems.com>
2024-06-25 17:38:46 +0400

    Application code - both Ruby and JavaScript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', () => {
+    const saveBtn = document.getElementById('click-me');
+
+    saveBtn.addEventListener('click', e => {
+        if (confirm('Are you sure?')) {
+            location.reload();
+        }
+    });
+});
