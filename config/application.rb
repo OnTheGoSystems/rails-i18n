b85bc75db08c111e991462009062c16cf462fb47
Ivan Larionov <ivan.l@onthegosystems.com>
2024-06-26 10:29:50 +0400

    Setup i18n

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module SampleRailsI18n
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :en
+    I18n.available_locales = [:en, :es, :de]
   end
 end
