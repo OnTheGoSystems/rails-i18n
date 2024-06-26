8547cd69f8ffedf5f939dd59cc410793931c27b2
Ivan Larionov <ivan.l@onthegosystems.com>
2024-06-26 11:32:04 +0400

    Internationalize JavaScript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { I18n } from "i18n-js"
+import { loadLocale } from './load_locale'
 
-document.addEventListener('turbo:load', () => {
+document.addEventListener('turbo:load', async () => {
+    const translations = await loadLocale();
+    const i18n = new I18n(translations);
+    i18n.locale = document.body.dataset['locale'];
+
     const saveBtn = document.getElementById('click-me');
 
     saveBtn.addEventListener('click', e => {
-        if (confirm('Are you sure?')) {
+        if (confirm(i18n.t('confirm'))) {
             location.reload();
         }
     });
